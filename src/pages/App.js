8d047d46718cb75159a802e55c17ced8fb93c8df
Kellie Matteson <kelliematteson@yahoo.com>
2021-03-16 16:34:35 -0700

    form in tweetfeed added

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,23 +1,17 @@
 import '../App.css';
-import Tweet from './Tweet';
+import TweetFeed from '../components/TweetFeed';
 
 function App() {
   return (
     <div className="App">
       <nav>
-        {/* Marsallis - NavBar component*/}
+        {/* Marsallis */}
       </nav>
       <section id="tweets-section">
-<<<<<<< HEAD
-        <Tweet />
-=======
-        {/* Kellie - TweetFeed component */}
-      </section>
-      <section id="tweet-show-page-section">
->>>>>>> 704bb214bf378c3dc3052b68545d800fa6a986ad
+        <TweetFeed />
       </section>
       <section id="news-section">
-        {/* Ryan - NewsFeed component*/}
+        {/* Ryan */}
       </section>
     </div>
   );
