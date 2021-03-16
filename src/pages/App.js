f5a1abb92760958b50b91740244c7dd1ce0472c5
Kevin Mathews <kev.math@yahoo.com>
2021-03-16 14:53:02 -0700

    1st commit

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,23 +1,19 @@
-import logo from '../logo.svg';
 import '../App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        {/* Marsallis - NavBar component*/}
+      </nav>
+      <section id="tweets-section">
+        {/* Kellie - TweetFeed component */}
+      </section>
+      <section id="tweet-show-page-section">
+      </section>
+      <section id="news-section">
+        {/* Ryan - NewsFeed component*/}
+      </section>
     </div>
   );
 }
