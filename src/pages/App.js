ca6c791582d698e5234f32e4c40fdbf60d78a578
Kellie Matteson <kelliematteson@yahoo.com>
2021-03-15 19:56:20 -0700

    made a tweet form

--- src/pages/App.js
+++ src/pages/App.js
@@ -7,6 +7,19 @@ function App() {
         {/* Marsallis */}
       </nav>
       <section id="tweets-section">
+        <h1>Latest and Greatest Tweets</h1>
+        <form id="tweet_form">
+            <label>Title
+              <input type="text" name="title" />
+            </label>
+            <label>Content
+              <input type="text" name="content" />
+            </label>
+            <label>Author
+              <input type="text" name="author" />
+            </label>
+            <input type="submit" value="Submit" />
+        </form>
         {/* Kellie */}
       </section>
       <section id="news-section">
