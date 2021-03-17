d0d493a8db7fa01686932d56439c5b4acaac0a3d
Parker <phmd13@gmail.com>
2021-03-16 22:58:39 -0400

    park commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,1 +1,104 @@
 // Parker
+
+// Pasted from code pen. I'll update and reformat once I get power again
+ 
+//  <div class ="container">
+//   <div class="Team"><h1>About</h1> 
+//   </div>
+
+//  <div class="company"><h2>Our Company</h2></div>
+//   <h4>Tweeter is a service committed to diversity in its many forms. Twitter is a community</br> oriented platform that is home to a variety of opinions, people, insights and backgrounds.</br> It was founded by Team Goat in 2021 in the SEIR-Ruth Cohort of General Assembly</br> by  Kellie, Parker, Kevin, Joseph, Andrea, Marsallis, and Ryan.</h4>
+// </div>
+
+// <div class="purpose"><h2>Our Mission</div>
+//  <h4>The Mission of Team Goat is to work as a team, execute our vision, and continue the legacy<br>of Goats that have come before us.</>
+// </div>
+
+// <div class="principles"><h2>Our Principles</div>
+//  <h3><li>Creating a community</li></h3>
+//   <h4>The purpose of Tweeter is to create a community that contributes their </br> own unique perspectives and ideas through our consinstently improving technology.</h4>
+//   <h3><li>Promoting Health</li></h3>
+//   <h4>Healthy conversations are a part of tweeter. Converse about sports, daily <br>happenings or anything that you can think of which can be healthy for users.</h4>
+//   <h3><li>Being Fun</li></h3>
+// <h4>Tweeter values being fun for its users, and Team Goat values having fun</br> while creating this project as a group.</h4>
+
+// <div class="team"><h2>Meet Team Goat</h2>
+// </div>
+// <h3><li>Kellie</h3></li>
+// <h4>Kellie is a contributing member of Team Goat and co-founder of tweeter.</h4>
+// <h3><li>Parker</h3></li>
+// <h4>Parker is a contributing member of Team Goat and co-founder of tweeter.</h4>
+// <h3><li>Kevin</h3></li>
+// <h4>Kevin is a contributing member of Team Goat and co-founder of tweeter.</h4>
+// <h3><li>Joseph</h3></li>
+// <h4>Joseph is a contributing member of Team Goat and co-founder of tweeter.</h4>
+// <h3><li>Andrea</h3></li>
+// <h4><li>Andrea is a contributing member of Team Goat and co-founder of tweeter.</h4></li>
+// <h3><li>Marsallis</h3></li>
+// <h4>Marsallisis a contributing member of Team Goat and co-founder of tweeter.</h4>
+// <h3><li>Ryan</h3></li>
+// <h4>Ryan is a contributing member of Team Goat and co-founder of tweeter.</h4>
+
+ 
+ 
+  
+    
+
+ 
+ 
+//    <div class="footer">
+//    <div class="footer-item"></div>
+//   About
+//     </a>
+//     <a class="footer-item">
+//       Help Center
+//     </>
+//     <a class="footer-item">
+//       Terms of Service
+//     </a>
+//     <a class="footer-item">
+//       Privacy Policy
+//     </a>
+//     <a class="footer-item">
+//       Cookie Policy
+//     </a>
+//     <a class="footer-item">
+//       Ads info
+//     </a>
+//     <a class="footer-item">
+//       Blog
+//     </a>
+//     <a class="footer-item">
+//       Status
+//     </a>
+//     <a class="footer-item">
+//       Careers
+//     </a>
+//     <a class="footer-item">
+//       Brand Resources
+//     </a>
+//     <a class="footer-item">
+//       Advertising
+//     </a>
+//     <a class="footer-item">
+//       Marketing
+//     </a>
+//     <a class="footer-item">
+//       Tweeter for Business
+//     </a>
+//     <a class="footer-item">
+//       Developers
+//     </a>
+//     <a class="footer-item">
+//       Directory
+//     </a>
+//     <a class="footer-item">
+//       Settings
+//     </a>
+//     <a class="footer-item">
+//       © 2021 Tweeter, Inc.
+//     </a>
+//   </div>
+//  </div>
+   
+   
