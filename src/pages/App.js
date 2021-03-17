ed1a96716ab9b9d74627803d458c3c223c6a6202
Marsallis <MarsallisF@gmail.com>
2021-03-16 20:05:36 -0400

    marsallis commit

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,9 +1,5 @@
 import '../App.css';
-<<<<<<< HEAD
 import NavBar from '../components/NavBar';
-=======
-import TweetFeed from '../components/TweetFeed';
->>>>>>> e28357bbc6f425021cec2915a02d471251756bb9
 
 function App() {
   return (
