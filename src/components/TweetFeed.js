740a80b8d4145baad86e569ae8a4a3727fec2e30
Kellie Matteson <kelliematteson@yahoo.com>
2021-03-17 15:31:32 -0700

    continue work on displaying tweetfeed

--- src/components/TweetFeed.js
+++ src/components/TweetFeed.js
@@ -1,23 +1,26 @@
-import { useEffect, useState } from 'react';
+import { useEffect} from 'react';
 export default function TweetFeed(props) {
-    const [tweets, setTweets] = useState[{}];
+    
+    
     useEffect( () => {
         const makeAPICall = async () => {
           try {
           const res = await fetch('http://localhost:3000/tweets');  
           const data = await res.json();
-          await setTweets(data);
-          console.log(setTweets);
+          
+          console.log(data)
           } catch (err) {
              console.error(err)
           }
         }
         makeAPICall()
       }, [])
+    
+   
     return (   
         
         <div className="tweeter">
-            
+           
             <form className="tweet_form">
             {/* <label>
                 <input type="text" name="title" />
