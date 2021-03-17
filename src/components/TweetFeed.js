6888aebe24e5da54bb23a34760566e5032269355
Kellie Matteson <kelliematteson@yahoo.com>
2021-03-16 18:20:06 -0700

    working on fetching data

--- src/components/TweetFeed.js
+++ src/components/TweetFeed.js
@@ -1,10 +1,13 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 export default function TweetFeed(props) {
+    const [tweets, setTweets] = useState[{}];
     useEffect( () => {
         const makeAPICall = async () => {
           try {
-          const res = await fetch('http://localhost:3000/tweets')  
-          const json = await res.json()
+          const res = await fetch('http://localhost:3000/tweets');  
+          const data = await res.json();
+          await setTweets(data);
+          console.log(setTweets);
           } catch (err) {
              console.error(err)
           }
@@ -12,7 +15,9 @@ export default function TweetFeed(props) {
         makeAPICall()
       }, [])
     return (   
+        
         <div className="tweeter">
+            
             <form className="tweet_form">
             {/* <label>
                 <input type="text" name="title" />
