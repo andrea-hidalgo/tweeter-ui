6dc776234ad58a2787cce47cc8c82714e6363bf6
Parker <phmd13@gmail.com>
2021-03-17 21:04:18 -0400

    about page updates

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,104 +1,88 @@
-// Parker
+<>
+  <div className="about">
+    <h1>About</h1> 
+  </div>
+  <div className="principles"><h2>Our Company</h2></div>
+  <h4>Tweeter is a service committed to diversity in its many forms. Twitter is a community oriented platform that is home to a variety of opinions, people, insights and backgrounds. It was founded by Team Goat in 2021 in the SEIR-Ruth Cohort of General Assembly by  Kellie, Parker, Kevin, Joseph, Andrea, Marsallis, and Ryan.</h4>
+  <div className="purpose"><h2>Our Mission</h2></div>
+  <h4>The Mission of Team Goat is to work as a team, execute our vision, and continue the legacy<br />of Goats that have come before us.</h4>
+  <div className="principles"><h2>Our Principles</h2></div>
+  <h3><ul>Creating a community</ul></h3>
+  <h4>The purpose of Tweeter is to create a community that contributes their <br /> own unique perspectives and ideas through our consinstently improving technology.</h4>
+  <h3><li>Promoting Health</li></h3>
+  <h4>Healthy conversations are a part of tweeter. Converse about sports, daily <br />happenings or anything that you can think of which can be healthy for users.</h4>
+  <h3><li>Being Fun</li></h3>
+  <h4>Tweeter values being fun for its users, and Team Goat values having fun<br /> while creating this project as a group.</h4>
+  <div className="team"><h2>Meet Team Goat</h2>
+  </div>
+  <h3><li>Kellie's Bio:</li></h3>
+  <h4> Kellie is an actor who codes. It is an easy fit for her because the two require a similar approach. Acting allows Kellie to find her own way into a character. With programming, she can step into challenges with an open mind and leverage the tools available to creatively problem solve and breathe life into products.</h4>
+  <h3><li>Parker's Bio</li></h3>
+  <h4>Parker functions with an exemplary work ethic and resolve to complete tasks proffesionally and timely. While remaining focused on the task at hand, he remains open to increasing his knowledge, experience and honing his skillset as an entrepreneur and developer.</h4>
+  <h3><li>Kevin Bio:</li></h3>
+  <h4>Kevin is a web developer with a background in sales and entrepreneurship. As a business owner, Kevin learned how to handle and resolve a wide range of issues which now enables him to approach projects with confidence and creativity. He brings his passion for a clean and organized work product into each project, and he loves nothing more then working as a team to find the most efficient and elegant solutions to complex problems.</h4>
+  <h3><li>Joseph Bio:</li></h3>
+  <h4>Joseph is an experienced Electrical Project Engineer with a demonstrated work history in major public works & infrastructure projects in NYC. </h4>
+  <h3><li>Andrea's Bio:</li></h3>
+  <h4><li>Andrea is a Software Engineer Fellow with a strong background in technical customer service.</li></h4>
+  <h3><li>Marsallis' Bio:</li></h3>
+  <h4>Marsallis is a Software Engineer who is strong-willed, determined, team-oriented, focused and willing to learn. M passion for development stems from helping others, especially my family- to help grow their businesses into successful modern day corporations. Marsallis fell in love with development once he realized he had all the tools to set other people up for success and to be able to see a smile on someone's face, as they see an application or a website customizable and personable to them.</h4>
+  <h3><li>Ryan's Bio</li></h3>
+  <h4>Ryan is an experienced digital producer, content creator and problem-solver for companies like Golf Channel, Disney World and Red Hat. He loves creating great user experiences through web production and coding.</h4>
+  <div className="footer">
+    <div className="footer-item">
+      About
+    </div>
+    <a className="footer-item">
+      Help Center
+    </a>
+    <a className="footer-item">
+      Terms of Service
+    </a>
+    <a className="footer-item">
+      Privacy Policy
+    </a>
+    <a className="footer-item">
+      Cookie Policy
+    </a>
+    <a className="footer-item">
+      Ads info
+    </a>
+    <a className="footer-item">
+      Blog
+    </a>
+    <a className="footer-item">
+      Status
+    </a>
+    <a className="footer-item">
+      Careers
+    </a>
+    <a className="footer-item">
+      Brand Resources
+    </a>
+    <a className="footer-item">
+      Advertising
+    </a>
+    <a className="footer-item">
+      Marketing
+    </a>
+    <a className="footer-item">
+      Tweeter for Business
+    </a>
+    <a className="footer-item">
+      Developers
+    </a>
+    <a className="footer-item">
+      Directory
+    </a>
+    <a className="footer-item">
+      Settings
+    </a>
+    <a className="footer-item">
+      © 2021 Tweeter, Inc.
+    </a>
+  </div>
+  export default About;
+</>
 
-// Pasted from code pen. I'll update and reformat once I get power again
- 
-//  <div class ="container">
-//   <div class="Team"><h1>About</h1> 
-//   </div>
-
-//  <div class="company"><h2>Our Company</h2></div>
-//   <h4>Tweeter is a service committed to diversity in its many forms. Twitter is a community</br> oriented platform that is home to a variety of opinions, people, insights and backgrounds.</br> It was founded by Team Goat in 2021 in the SEIR-Ruth Cohort of General Assembly</br> by  Kellie, Parker, Kevin, Joseph, Andrea, Marsallis, and Ryan.</h4>
-// </div>
-
-// <div class="purpose"><h2>Our Mission</div>
-//  <h4>The Mission of Team Goat is to work as a team, execute our vision, and continue the legacy<br>of Goats that have come before us.</>
-// </div>
-
-// <div class="principles"><h2>Our Principles</div>
-//  <h3><li>Creating a community</li></h3>
-//   <h4>The purpose of Tweeter is to create a community that contributes their </br> own unique perspectives and ideas through our consinstently improving technology.</h4>
-//   <h3><li>Promoting Health</li></h3>
-//   <h4>Healthy conversations are a part of tweeter. Converse about sports, daily <br>happenings or anything that you can think of which can be healthy for users.</h4>
-//   <h3><li>Being Fun</li></h3>
-// <h4>Tweeter values being fun for its users, and Team Goat values having fun</br> while creating this project as a group.</h4>
-
-// <div class="team"><h2>Meet Team Goat</h2>
-// </div>
-// <h3><li>Kellie</h3></li>
-// <h4>Kellie is a contributing member of Team Goat and co-founder of tweeter.</h4>
-// <h3><li>Parker</h3></li>
-// <h4>Parker is a contributing member of Team Goat and co-founder of tweeter.</h4>
-// <h3><li>Kevin</h3></li>
-// <h4>Kevin is a contributing member of Team Goat and co-founder of tweeter.</h4>
-// <h3><li>Joseph</h3></li>
-// <h4>Joseph is a contributing member of Team Goat and co-founder of tweeter.</h4>
-// <h3><li>Andrea</h3></li>
-// <h4><li>Andrea is a contributing member of Team Goat and co-founder of tweeter.</h4></li>
-// <h3><li>Marsallis</h3></li>
-// <h4>Marsallisis a contributing member of Team Goat and co-founder of tweeter.</h4>
-// <h3><li>Ryan</h3></li>
-// <h4>Ryan is a contributing member of Team Goat and co-founder of tweeter.</h4>
-
- 
- 
-  
-    
-
- 
- 
-//    <div class="footer">
-//    <div class="footer-item"></div>
-//   About
-//     </a>
-//     <a class="footer-item">
-//       Help Center
-//     </>
-//     <a class="footer-item">
-//       Terms of Service
-//     </a>
-//     <a class="footer-item">
-//       Privacy Policy
-//     </a>
-//     <a class="footer-item">
-//       Cookie Policy
-//     </a>
-//     <a class="footer-item">
-//       Ads info
-//     </a>
-//     <a class="footer-item">
-//       Blog
-//     </a>
-//     <a class="footer-item">
-//       Status
-//     </a>
-//     <a class="footer-item">
-//       Careers
-//     </a>
-//     <a class="footer-item">
-//       Brand Resources
-//     </a>
-//     <a class="footer-item">
-//       Advertising
-//     </a>
-//     <a class="footer-item">
-//       Marketing
-//     </a>
-//     <a class="footer-item">
-//       Tweeter for Business
-//     </a>
-//     <a class="footer-item">
-//       Developers
-//     </a>
-//     <a class="footer-item">
-//       Directory
-//     </a>
-//     <a class="footer-item">
-//       Settings
-//     </a>
-//     <a class="footer-item">
-//       © 2021 Tweeter, Inc.
-//     </a>
-//   </div>
-//  </div>
-   
-   
+export default About;
