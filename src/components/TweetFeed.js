3de09ace156669244b5323497dcc0ae238b19622
Joseph <joseph.hatzilazaridis@gmail.com>
2021-03-18 19:19:46 -0400

    tweet posting to tweetfeed working

--- src/components/TweetFeed.js
+++ src/components/TweetFeed.js
@@ -1,20 +1,69 @@
 import '../css/TweetFeed.css'
+import { useState, useEffect, useRef } from 'react';
 
 export default function TweetFeed(props) {
+    
+    const [tweets, setTweets] = useState([]);
+    const input = useRef(null);
+    useEffect( () => {
+        const makeAPICall = async () => {
+          try {
+          const res = await fetch('https://tweeter-api-goat.herokuapp.com/tweets');  
+          const data = await res.json();
+          setTweets(data.tweets);
+          } catch (err) {
+             console.error(err)
+          }
+        }
+        makeAPICall()
+      }, []);
+
+    const handleSubmit = async event => {
+        event.preventDefault();
+        const value = input.current.value;
+        try {
+            const response = await fetch('https://tweeter-api-goat.herokuapp.com/tweets', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                     content: value
+                })
+            });
+            const data = await response.json();
+            setTweets([...tweets, data.tweet]);
+            input.current.value = '';
+        } catch (error) {
+            console.error(error);
+        }
+    };
+       
     return (   
+        <>
         <div className="tweeter">
-            <form className="tweet_form">
-            {/* <label>
-                <input type="text" name="title" />
-            </label> */}
-                <label>
-                    <input type="text" name="content" placeholder="What's happening?" />
+           
+            <form className="tweet_form" onSubmit={handleSubmit}>
+                <label className="label_form">
+                    <input type="text" name="content" ref={input} placeholder="What's happening?" />
                 </label>
-            {/* <label>
-                <input type="text" name="author" />
-            </label> */}
+            <input className="tweet_button" type="submit" value="Tweet" />
             </form>
-            <button className="tweet_button" type="submit" value="Submit">Tweet</button>
+            <div className="tweet_feed">
+                <ul>
+                    {tweets.map((tweet, id) => {
+                        return (          
+                            <li key={id}>
+                                {tweet.author}
+                                {tweet.title}
+                                {tweet.content}
+                            </li>
+                        )
+                    })}      
+                </ul>
+            </div>
+            
         </div>
+        </>
     )
 }
