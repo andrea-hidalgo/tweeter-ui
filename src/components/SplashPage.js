d497346deedf912d43057eec4ac316b65759c168
Kevin Mathews <kev.math@yahoo.com>
2021-03-19 00:20:44 -0700

    changed app name to Tweeter, added favicon

--- src/components/SplashPage.js
+++ src/components/SplashPage.js
@@ -1,22 +1,18 @@
-import { useState, useEffect, useRef } from 'react';
 import '../css/SplashPage.css';
 import '../css/App.css';
 export default function SplashPage() {
 
 
     return(
-        <div className='splash-page-container'>
+    <div className='splash-page-container'>
         <div className='splash-page'> 
-    
-        <div class='image-container'>
-            <img class='title-image' src='https://i.imgur.com/jbhj59U.gif'></img>
-        </div><br />
-        <div class='enter-container'>
-            <img class='enter-image' src='https://i.imgur.com/WfNuem7.gif'
-            //  onClick={handleToggle}
-             ></img>
-            
-        </div> 
+            <div class='image-container'>
+                <img class='title-image' src='https://i.imgur.com/jbhj59U.gif'></img>
+            </div>
+            <br />
+            <div class='enter-container'>
+                <img class='enter-image' src='https://i.imgur.com/WfNuem7.gif'></img>
+            </div> 
         </div>
    </div>      
 )
--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Switch>
-      <Route path={"/main"} component={App}/>
+        <Route path={"/main"} component={App}/>
         <Route path={'/about'} component={About}/>
         <Route path={"/"} component={SplashPage}/>
         
