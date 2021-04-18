7df591cf71425f4250ff2a2838d8865339e25c94
Kevin Mathews <kev.math@yahoo.com>
2021-04-18 16:20:12 -0700

    working on tweetpage responsiveness

--- src/pages/App.js
+++ src/pages/App.js
@@ -5,6 +5,8 @@ import NavBar from '../components/NavBar';
 import NewsFeed from '../components/NewsFeed';
 import TweetShowPage from '../components/TweetShowPage';
 import SplashPage from '../components/SplashPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Grid, Row, Col } from 'react-bootstrap';
 
 function App() {
 
@@ -28,14 +30,18 @@ function App() {
 
   return (
     <div className="App">
-      
+      <Row>
+      <Col className='d-lg-none d-md-none d-sm-none d-xs-none'>
       <nav>
         <NavBar/>
       </nav>
+      </Col>
       <div className="app-right">
+      <Col md='auto'>
         <section id="tweets-section">
           <TweetFeed handleTweetShowPage={handleTweetShowPage}/>
         </section>
+        
         {showPageHidden.showPageHidden === false ? (
           <section id="tweet-show-page-section">
             <TweetShowPage 
@@ -46,11 +52,17 @@ function App() {
 				) : (
 					''
 				)}
+        </Col> 
+        <Col className='d-lg-block d-md-none d-sm-none d-xs-none'>
         <section id="news-section">
           <NewsFeed />
         </section>
+        </Col>
       </div>
+     
+      </Row>
     </div>
+    
   )
 }
 
